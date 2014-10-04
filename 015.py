84ff2440d61b40252741b2c317144245938af723
mangafan92 <themangafan92@gmail.com>
2014-10-04 17:44:01 +0200

    Correction et ajout de la possiblité de choisir la longueur du côté du carré

--- 015.py
+++ 015.py
@@ -2,18 +2,16 @@ def nombreRoutes(a, b, table):
     if table[a][b] != 0:
         return table[a][b]
     else:
-        if a == b:
-            out = 2*nombreRoutes(a, b-1, table)
-            table[a][b] = out             
-            return out
         if a == 0 or b == 0:
             return 1
         out = nombreRoutes(a-1, b, table) + nombreRoutes(a, b-1, table)
         table[a][b] = out
         return out
 
-grid = []
-for k in range(0,21):
-    grid.append([0]*21)
+nombre = int(input("Nombre:"))
 
-print(nombreRoutes(20,20, grid))
+grid = []
+for k in range(0,nombre+1):
+    grid.append([0]*(nombre+1))
+
+print("Le nombre de chemins différents qui permettent d'aller d'un coin au point opposé d'un carré de", nombre, "cotés est", nombreRoutes(nombre, nombre, grid), ".")
