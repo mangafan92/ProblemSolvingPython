cc1e71d3a67b2663c6fc9c01d59fb5ef61c9fd23
mangafan92 <themangafan92@gmail.com>
2014-10-04 13:24:33 +0200

    Ajout possibilité modifier borne

--- 001.py
+++ 001.py
@@ -1,7 +1,8 @@
-s=0
+borne = int(input("Nombre:"))
+s = 0
 
-for k in range (1,1000):
+for k in range (1,borne):
     if k % 3 == 0 or k % 5 == 0:
         s+=k
         
-print("La sommme des multiples de 3 ou 5 inférieurs à 1000 est", s, ".")
+print("La sommme des multiples de 3 ou 5 inférieurs à", borne, "est", s, ".")
