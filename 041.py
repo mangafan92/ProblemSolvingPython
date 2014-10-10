ea66bcea2d7884edcebbdc1f6fb417c2d4c3c37b
mangafan92 <themangafan92@gmail.com>
2014-10-10 22:28:40 +0200

    041 corrigé

--- 041.py
+++ 041.py
@@ -7,27 +7,38 @@ def estPremier(n):
             return False
     return True
 
-def reorganiser(elements, liste, debut):
-    if len(elements) == 1:
+def reorganiser(elements, liste, debut, apparitionsMultiples, longueur):
+    if longueur == 1 and len(elements) == 1:
         liste.append(debut + elements[0])
-    else:
+    elif longueur == 1:
         for k in range(0, len(elements)):
+            sortie = debut + elements[k]
+            liste.append(sortie)
+    else:
+        for k in range(0, len(elements)):         
+            debutTemp = debut
             elementsTemp = list(elements)
-            debutTemp = debut           
-            debutTemp += elements[k]
-            elementsTemp.remove(elements[k])
             
-            reorganiser(elementsTemp, liste, debutTemp)
-        
+            debutTemp += elementsTemp[k]
             
-liste = []
-elements = []
-for k in range(1, 8):
-    elements.append(str(k))
+            if apparitionsMultiples == False:
+                elementsTemp.remove(elements[k])
+            reorganiser(elementsTemp, liste, debutTemp, apparitionsMultiples, longueur-1)
+            
+def annagramme(elements, apparitionsMultiples, longueur):
+    liste = []
+    debut = ""
+    reorganiser(elements, liste, debut, apparitionsMultiples, longueur) 
+    return liste  
     
-debut = ""
+liste = []
 
-reorganiser(elements, liste, debut)
+for k in range(2, 9):
+    elements = []
+    for k in range(1, k):
+        elements.append(str(k))
+    
+    liste += annagramme(elements, False, len(elements))
 
 listePremiers = []
 
@@ -37,4 +48,4 @@ for k in range(0, len(liste)):
         
 listePremiers.sort()
 
-print(listePremiers)
+print("Le plus grand nombre premier pandigital est ", listePremiers[len(listePremiers)-1], ".", sep="")
