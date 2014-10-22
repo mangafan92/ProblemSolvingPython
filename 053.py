36bfa5d04f113a0d0e63388c62a1ea9bdb08f9f2
mangafan92 <themangafan92@gmail.com>
2014-10-22 16:39:26 +0200

    053 ajouté

---
+++ 053.py
@@ -0,0 +1,19 @@
+def nCr(n, r, listeFactorielles):
+    return listeFactorielles[n]/listeFactorielles[r]/listeFactorielles[n-r]
+    
+def genererListeFactorielles(n):
+    liste = [1]
+    for n in range(1, n+1):
+        liste.append(liste[len(liste)-1]*n)
+    return liste
+
+listeFactorielles = genererListeFactorielles(100)
+k = 0
+borne = 100
+  
+for n in range(1, borne+1):
+    for r in range(1, n+1):
+        if nCr(n, r, listeFactorielles) > 10**6:
+            k += 1
+            
+print("Il y a", k, "coefficients.")
