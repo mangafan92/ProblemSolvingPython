d8966d60144a4a805c62e65510ef399959364edc
mangafan92 <themangafan92@gmail.com>
2014-12-14 13:46:23 +0100

    078 modifié, suppression des cas identiques au cours du calcul

--- 50-99/078.py
+++ 50-99/078.py
@@ -1,26 +1,46 @@
 def startrecur(coins):
     partition = []
     piles = []
-    recur(coins, partition, piles)
-    for pile in piles:
-        pile.sort()
-    cleanpiles(piles)
+    recur(coins, piles)
+    while not isover(piles, coins):
+        recur(coins, piles)
+        cleanpiles(piles)
     return piles
 
-def recur(coins, partition, piles):
-    if coins > 0:
-        for k in range(1, coins+1):
-            tmppartition = list(partition)
-            tmppartition.append(k)
-            recur(coins-k, tmppartition, piles)
+def recur(coins, piles):
+    if not piles == []:
+        for pile in piles:
+            if sum(pile) < coins:
+                for k in range(1, coins - sumpile(pile) + 1):
+                    tmppile = list(pile)
+                    tmppile.append(k)
+                    piles.append(tmppile)
+                piles.remove(pile)
+                break
     else:
-        piles.append(partition)
+        for k in range(1, coins+1):
+            piles.append([k])
 
 def cleanpiles(piles):
+    for pile in piles:
+        pile.sort()
     tmppiles = list(piles)
     for pile in tmppiles:
         while piles.count(pile) > 1:
             piles.remove(pile)
 
-piles = startrecur(7)
-print(piles, len(piles))
+def isover(piles, coins):
+    for pile in piles:
+        if sumpile(pile) < coins:
+            return False
+    return True
+
+def sumpile(pile):
+    sum = 0
+    for number in pile:
+        sum += number
+    return sum
+
+for k in range(4, 21):
+    piles = startrecur(k)
+    print(k, len(piles), piles)
