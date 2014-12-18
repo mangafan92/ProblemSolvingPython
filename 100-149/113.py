f1c59c4d4190be32f30f2147bb9ca9eabe1614a8
mangafan92 <themangafan92@gmail.com>
2014-12-18 18:00:40 +0100

    113 modifié (plus rapide mais toujours pas assez pour 10^100)

--- 100-149/113.py
+++ 100-149/113.py
@@ -1,32 +1,38 @@
+# TODO
+
 def startrecur(length):
-    numbers = list()
+    numbers = [0]
     for k in range(0, 10):
-        increasing(str(k), length-1, numbers)
-        decreasing(str(k), length-1, numbers)
+        increasing(k, length-1, numbers)
+
+    for k in range(1, 10):
+        decreasing(k, length - 1, False, numbers)
+    decreasing(0, length - 1, True, numbers)
+
     return numbers
 
 def increasing(number, length, numbers):
     if length > 0:
-        for k in range(int(number)%10, 10):
-            increasing(number + str(k), length-1, numbers)
+        for k in range(number, 10):
+            increasing(k, length-1, numbers)
     else:
-        if not int(number) in numbers:
-            numbers.append(int(number))
+        numbers[0] += 1
 
-def decreasing(number, length, numbers):
+def decreasing(number, length, start, numbers):
     if length > 0:
-        if int(number) == 0:
-            for k in range(0, 10):
-                decreasing(number + str(k), length-1, numbers)
+        if start:
+            decreasing(0, length - 1, True, numbers)
+            for k in range(1, 10):
+                decreasing(k, length - 1, False, numbers)
         else:
-            for k in range(0, int(number)%10+1):
-                decreasing(number + str(k), length-1, numbers)
+            for k in range(0, number+1):
+                decreasing(k, length - 1, False, numbers)
     else:
-        if not int(number) in numbers:
-            numbers.append(int(number))
+        numbers[0] += 1
 
 if __name__ == '__main__':
-    numbers = startrecur(6)
+    length = 6
+    numbers = startrecur(length)
     numbers.sort()
-    print(len(numbers))
-    print(numbers)
+    # On enlève tous les nombes qui sont croissants et décroissants, ainsi que les 2 fois où 0 apparait
+    print(numbers[0] - 9*length - 2)
