bd8507a3fd636a8671e039185cf92cf6628ef94a
mangafan92 <themangafan92@gmail.com>
2015-03-07 18:52:27 +0100

    021 réécrit

--- 001-049/021.py
+++ 001-049/021.py
@@ -1,33 +1,19 @@
-def listeDiviseurs(n):
-    liste = []
-    for k in range(1, n):
-        if n % k == 0:
-            liste.append(k)
-            
-    return liste
-    
-def sommeDiviseurs(n, table):
-    if table[n] == 0:    
-        liste = listeDiviseurs(n)
-        s = 0
-        for k in range(0, len(liste)):
-            s += liste[k]
-        table[n] = s
-    return table[n]
-            
-nombre = int(input("Nombre:"))
-listeNombresAmicaux = []
-tableSommeDiviseurs = [0] * (nombre+1)
+import modules.primes
 
-for i in range(1, nombre+1):
-    for j in range(i+1, nombre+1):
-        if sommeDiviseurs(i, tableSommeDiviseurs) == j and sommeDiviseurs(j, tableSommeDiviseurs) == i:         
-            listeNombresAmicaux.append(i)                
-            listeNombresAmicaux.append(j)
+primes = modules.primes.Primes()
 
-s = 0
+def solveProblem(limit=10000):
+    sums = [0] + list(map(lambda n: sum(primes.divisors(n))-n, range(1, limit+1)))
+    amical = list()
+    for number in range(1, limit+1):
+        try:
+            if number == sums[sums[number]] and sums[number] != number:
+                amical.append(number)
+        except:
+            pass
 
-for k in range(0, len(listeNombresAmicaux)):
-    s += listeNombresAmicaux[k]
-    
-print("La somme des nombres amicaux inférieurs à", nombre, "vaut", s, ".")            
+    result = sum(amical)
+    return result
+
+if __name__ == '__main__':
+    print(solveProblem())
