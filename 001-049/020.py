c18ae94fcec6bb4c80fbbe7c5029d320a2b6f854
mangafan92 <themangafan92@gmail.com>
2015-03-07 19:02:30 +0100

    020 réécrit

--- 001-049/020.py
+++ 001-049/020.py
@@ -1,14 +1,14 @@
-def factorielle(n):
+def factorial(n):
     if n == 0 or n == 1:
         return 1
     else:
-        return n*factorielle(n-1)
-        
-nombre = int(input("Nombre:"))
-factorielle = str(factorielle(nombre))
-s = 0
+        return n*factorial(n-1)
 
-for k in range(0, len(factorielle)):
-    s += int(factorielle[k])
-    
-print(nombre, "factorielle est égal à", factorielle, "et la somme des chiffres qui le composent est", s, ".")
+def solveProblem(number=100):
+    number = factorial(number)
+    number = list(str(number))
+    number = map(int, number)
+    return sum(number)
+
+if __name__ == '__main__':
+    print(solveProblem())
