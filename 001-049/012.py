ffc862ad97e3017567fca6986962914e6b3321f2
mangafan92 <themangafan92@gmail.com>
2015-03-07 17:53:56 +0100

    012 réécrit

--- 001-049/012.py
+++ 001-049/012.py
@@ -1,21 +1,15 @@
-def nombreDiviseurs(n):
-    diviseurs = 0
-    for k in range(1, n+1):
-        if n%k == 0:
-            diviseurs += 1
-    return diviseurs
-    
-borne = int(input("Nombre:"))
-k = 0
-n = 0
+import modules.primes
 
-while n < borne:
-# Le nombre de diviseurs de n(n-1)/2 est égal au produits du nombre de diviseurs de n/2 et de n+1 ou de n et (n+1)/2 car n et n+1 n'ont aucun facteur premier en commun    
-    if k % 2 == 0:
-        n = nombreDiviseurs(int(k/2)*nombreDiviseurs(k+1))
-    else:
-        n = nombreDiviseurs(k)*nombreDiviseurs(int((k+1)/2))
-    k += 1
-  
-k -= 1  
-print("Le premier nombre triangulaire à avoir plus de", borne, "diviseurs est", int(k*(k+1)/2), ".")
+primes = modules.primes.Primes()
+
+def triangular(n):
+    return n*(n+1)//2
+
+def solveProblem(limit=500):
+    k = 0
+    while primes.numberOfDivisors(triangular(k)) < limit:
+        k += 1
+    return triangular(k)
+
+if __name__ == '__main__':
+    print(solveProblem())
