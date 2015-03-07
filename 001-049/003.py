22828dd212bc2e88df3805e424e61cf802a6b95e
mangafan92 <themangafan92@gmail.com>
2015-03-07 17:52:47 +0100

    003 réécrit

--- 001-049/003.py
+++ 001-049/003.py
@@ -1,16 +1,13 @@
-n = int(input("Nombre:"))
-p = n
-k = 2
+def solveProblem(number=600851475143):
+    divisor = 2
 
-while p > 1:    
-    if p % k == 0:
-        p //= k
-    else:
-        k += 1
+    while number > 1:
+        if number % divisor == 0:
+            number //= divisor
+        else:
+            divisor += 1
 
-print("Le plus grand diviseur premier de", n, "est", k)
+    return divisor
 
-# Le plus petit diviseur positif et supérieur à 1 d'un nombre est un nombre premier.
-# Lorsque l'on divise p par k, k est le plus petit diviseur de p donc k est premier.
-# De plus, la valeur de k augmente au fur et à mesure de l'exécution du programme, la dernière valeur prise par k est donc la plus grande qu'il prend.
-# Elle est par conséquent le plus grand diviseur premier de n.
+if __name__ == '__main__':
+    print(solveProblem())
