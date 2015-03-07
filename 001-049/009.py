d34687a0b741d441cb2f5869c55c1c4dc094ebcd
mangafan92 <themangafan92@gmail.com>
2015-03-07 17:53:33 +0100

    009 réécrit

--- 001-049/009.py
+++ 001-049/009.py
@@ -1,25 +1,8 @@
-# Pour certaines valeurs, il y a plusieurs triplets pythagoriciens qui fonctionnent, par exemple pour nombre=1500
+def solveProblem(number=1000):
+    for a in range(1, number+1):
+        for b in range(a+1, number+1):
+            if a + b + (a**2 + b**2)**(1/2) == number:
+                return int(a*b*(a**2 + b**2)**(1/2))
 
-nombre = int(input("Nombre:"))
-produits = []
-valeursA = []
-valeursB = []
-valeursC = []
-
-for a in range(1, nombre):
-    for b in range (a, nombre):
-        c = (a**2 + b**2)**(1/2)       
-        if int(c) == c and a + b + c == nombre:
-            c = int(c)            
-            valeursA.append(a)
-            valeursB.append(b)
-            valeursC.append(c)
-            produits.append(a*b*c)
-            
-if len(produits) != 0:            
-    print("Il existe au moins un triplet un triplet pythagoricien tel que a+b+c =", nombre, ".")
-    print("Les valeurs de a, b et c qui fonctionnent ainsi que de leurs produits sont:")
-    for k in range(0, len(produits)):
-        print("a =", valeursA[k], "b =", valeursB[k], "c =", valeursC[k], "a*b*c =", produits[k])        
-else:
-    print("Il n'existe pas de triplet pythagoricien tel que a+b+c =", nombre, ".")
+if __name__ == '__main__':
+    print(solveProblem())
