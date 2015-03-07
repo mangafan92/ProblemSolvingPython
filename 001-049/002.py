69e3fd436b3107ad20ed0a48f61faa77ca29193a
mangafan92 <themangafan92@gmail.com>
2015-03-07 17:52:39 +0100

    002 réécrit

--- 001-049/002.py
+++ 001-049/002.py
@@ -1,30 +1,25 @@
 def fibonacci(n):
-    U1 = 1
-    U2 = 2
-    
-    if n == 1:
-        return 1
-    elif n == 2:
-        return 2
-    elif n > 2:    
-        for k in range(0, n-2):
-            temp = U2
-            U2 = U2 +U1
-            U1 = temp
-            
-        return U2
-    else:
-        return 0;
+    def fibonacciRecur(a, b, n):
+        if n == 0:
+            return b
+        else:
+            return fibonacciRecur(b, a+b, n-1)
 
-borne = int(input("Nombre:"))
-s = 0
-n = 1
-fn = 0
+    return fibonacciRecur(1, 1, n)
 
-while fn < borne:
-    fn = fibonacci(n)   
-    if fn < borne and fn % 2 == 0:
-        s += fn
-    n+=1
-    
-print("La somme des termes pairs et inférieurs à", borne, "de la suite de Fibonacci est", s,".")
+def solveProblem(limit=4*10**6):
+    result = 0
+    n = 0
+
+    fn = fibonacci(n)
+
+    while fn < limit:
+        if fn%2 == 0:
+            result += fn
+        n += 1
+        fn = fibonacci(n)
+
+    return result
+
+if __name__ == '__main__':
+    print(solveProblem())
