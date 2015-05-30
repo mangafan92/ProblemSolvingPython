48f96358cec149310959e982f3cecbc9c5935c7e
mangafan92 <themangafan92@gmail.com>
2015-05-30 16:34:45 +0200

    002 réécrit

--- 001-049/002.py
+++ 001-049/002.py
@@ -1,25 +1,16 @@
-def fibonacci(n):
-    def fibonacciRecur(a, b, n):
-        if n == 0:
-            return b
-        else:
-            return fibonacciRecur(b, a+b, n-1)
+def fibonacciDyn(limit):
+    output = [1, 2]
+    while output[-1] <= limit:
+        output.append(output[-1] + output[-2])
+    return output[:-1]
 
-    return fibonacciRecur(1, 1, n)
 
-def solveProblem(limit=4*10**6):
-    result = 0
-    n = 0
+def solveProblem(limit=4 * 10 ** 6):
+    terms = fibonacciDyn(limit)
+    isEven = lambda n: n % 2 == 0
+    terms = filter(isEven, terms)
+    return sum(terms)
 
-    fn = fibonacci(n)
-
-    while fn < limit:
-        if fn%2 == 0:
-            result += fn
-        n += 1
-        fn = fibonacci(n)
-
-    return result
 
 if __name__ == '__main__':
     print(solveProblem())
