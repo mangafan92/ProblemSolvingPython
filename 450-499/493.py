bb50817e8d67092ab615b651225eaedd611ada39
mangafan92 <themangafan92@gmail.com>
2015-11-12 20:18:44 +0100

    correction mise en forme code + typage fonctions

--- 450-499/493.py
+++ 450-499/493.py
@@ -1,18 +1,20 @@
-def distinctColors(balls:int, ballsToPick: int, colors:int):
-    ballsPerColor = balls//colors
+def distinctColors(balls: int, ballsToPick: int, colors: int) -> float:
+    ballsPerColor = balls // colors
 
-    def distinctColorsRec(ballsLeftToPick: int, currentDistinctColors: int, ballsLeft: int, ballsDistinctColorLeft: int):
+    def distinctColorsRec(ballsLeftToPick: int, currentDistinctColors: int, ballsLeft: int, ballsDistinctColorLeft: int) -> float:
         if ballsLeftToPick == 0 or ballsDistinctColorLeft == 0:
             return currentDistinctColors
         else:
-            output = ballsDistinctColorLeft/ballsLeft*distinctColorsRec(ballsLeftToPick-1, currentDistinctColors+1, ballsLeft-1, ballsDistinctColorLeft-ballsPerColor)
-            output += (1 - ballsDistinctColorLeft/ballsLeft)*distinctColorsRec(ballsLeftToPick-1, currentDistinctColors, ballsLeft-1, ballsDistinctColorLeft)
+            output = ballsDistinctColorLeft / ballsLeft * distinctColorsRec(ballsLeftToPick - 1, currentDistinctColors + 1, ballsLeft - 1, ballsDistinctColorLeft - ballsPerColor)
+            output += (1 - ballsDistinctColorLeft / ballsLeft) * distinctColorsRec(ballsLeftToPick - 1, currentDistinctColors, ballsLeft - 1, ballsDistinctColorLeft)
             return output
 
-    return distinctColorsRec(ballsToPick-1, 1, balls-1, balls-ballsPerColor)
+    return distinctColorsRec(ballsToPick - 1, 1, balls - 1, balls - ballsPerColor)
+
 
 def solve():
-    return round(distinctColors(70, 20, 7),9)
+    return round(distinctColors(70, 20, 7), 9)
+
 
 if __name__ == '__main__':
     print(solve())
