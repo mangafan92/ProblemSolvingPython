bb78d379457d726cb516d4fc008207902ce7f12a
mangafan92 <themangafan92@gmail.com>
2016-04-05 19:36:17 +0200

    145 modifié
    Donne la réponse en une dizaine de minutes maintenant

--- 100-149/145.py
+++ 100-149/145.py
@@ -1,30 +1,72 @@
-import itertools
+"""
+Principe:
+    - on construit par récurrence des nombres réversibles
+    - par exemple, un nombre à 5 chiffres s'écrit abcde, on va fixer les chiffres dans cet ordre: eadbc en s'assurant au fur et à mesure (tous les 2 chiffres fixés) que la fin de la somme du nombre et de son réversible a bien une fin qui s'écrit uniquement avec des impairs
+        - en effet, si a et e sont fixés, le dernier chiffre de la somme de abcde et de edcba est fixé
+    - ce programme ne respecte pas du tout la règle de la minute, il respecte néanmoins la règle des 10 minutes
+"""
 
-def decimal(number):
-    number = list(reversed(number))
-    result = 0
-    for k in range(len(number)):
-        result += number[k]*10**k
-    return result
 
-def isReversible(number):
-    reversedNumber = list(reversed(number))
-    number = decimal(number) + decimal(reversedNumber)
-    power = 0
-    while number // (10**power) > 0:
-        if number // (10**power) % 2 == 0:
+def reverse(n: int) -> int:
+    return int(str(n)[::-1])
+
+
+def addReverse(n: int) -> int:
+    return n + reverse(n)
+
+
+def isReversible(n: int, limit: int = None) -> bool:
+    """
+    :param n: entier
+    :param limit: nombre de digit à regarder au maximum dans addReverse(n), on ne regarder que les 3 derniers chiffres si limit = 3, si limit = None alors, on regarde tous les chiffres
+    :return: vérifie la définition ?
+    """
+    np = addReverse(n)
+    even = range(0, 10, 2)
+
+    limit = limit + 1 if limit is not None else len(str(np))
+
+    for k in range(0, limit):
+        if np // 10 ** k % 10 in even:
             return False
-        power += 1
     return True
 
-reversibles = 0
 
-for k in range(8):
-    for part1 in itertools.product(range(1, 10), repeat=2):
-        for part2 in itertools.product(range(10), repeat=k):
-            number = (part1[0],) + part2 + (part1[1],)
-            if isReversible(number):
-                print(number, reversibles)
-                reversibles += 1
+def reversibles(digits: int):
+    """
+    :param digits: nombre de digits voulus
+    :return: set contenant tous les nombres réversibles possèdant le nombre de chiffres voulu
+    """
+    output = set()
 
-print(reversibles)
+    def reversiblesRec(n: int, digitToSet: int) -> None:
+        """
+        :param n: entier qu'on est en train de construire
+        :param digitToSet: le chiffre à choisir (on va aussi choisir le chiffre (digits - digitToSet - 1))
+        :return: None
+        """
+        if digits % 2 == 0 and digitToSet == digits // 2:
+            if isReversible(n):
+                output.add(n)
+        elif digits % 2 == 1 and digitToSet == (digits - 1) // 2:
+            for k in range(10):
+                np = n + k * 10 ** digitToSet
+                if isReversible(np):
+                    output.add(np)
+        else:
+            for i in range(0 if digitToSet > 0 else 1, 10):
+                for j in range(0 if digitToSet > 0 else 1, 10):
+                    np = n + i * 10 ** digitToSet + j * 10 ** (digits - digitToSet - 1)
+                    if isReversible(np, digitToSet):
+                        reversiblesRec(np, digitToSet + 1)
+
+    reversiblesRec(0, 0)
+    return output
+
+
+def solve(limit: int = 9) -> int:
+    return sum(len(reversibles(k)) for k in range(0, limit + 1))
+
+
+if __name__ == '__main__':
+    print(solve())
