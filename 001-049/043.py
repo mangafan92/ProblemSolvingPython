80f360399caa43a4aeb4ead69fda78f34b758642
mangafan92 <themangafan92@gmail.com>
2016-04-17 10:30:05 +0200

    043 réécrit

--- 001-049/043.py
+++ 001-049/043.py
@@ -1,36 +1,16 @@
-def reorganiser(elements, liste, debut, apparitionsMultiples, longueur):
-    if longueur == 1 and len(elements) == 1:
-        liste.append(debut + elements[0])
-    elif longueur == 1:
-        for k in range(0, len(elements)):
-            sortie = debut + elements[k]
-            liste.append(sortie)
-    else:
-        for k in range(0, len(elements)):         
-            debutTemp = debut
-            elementsTemp = list(elements)
-            
-            debutTemp += elementsTemp[k]
-            
-            if apparitionsMultiples == False:
-                elementsTemp.remove(elements[k])
-            reorganiser(elementsTemp, liste, debutTemp, apparitionsMultiples, longueur-1)
-            
-def annagramme(elements, apparitionsMultiples, longueur):
-    liste = []
-    debut = ""
-    reorganiser(elements, liste, debut, apparitionsMultiples, longueur) 
-    return liste
-    
-elements = ""
-for k in range(0, 10):
-    elements += str(k)
+"""
+Principe:
+    - les nombres pandigitaux sont des nombres dont l'écriture en base 10 est une permutation des chiffres 0 à 9 ne commençant pas par 0
+    - il y en a un nombre pas trop grand, on peut tester la condition voulue pour chaque nombre en temps raisonnable
+"""
 
-liste = annagramme(elements, False, 10)
-somme= 0
+import itertools
 
-for element in liste:
-    if int(element[1:4])%2 == 0 and int(element[2:5])%3 == 0 and int(element[3:6])%5 == 0 and int(element[4:7])%7 == 0 and int(element[5:8])%11 == 0 and int(element[6:9])%13 == 0 and int(element[7:10])%17 == 0:
-        somme += int(element)
 
-print(somme)
+def solve() -> int:
+    ok = lambda e: e[0] != "0" and int(e[1:4]) % 2 == 0 and int(e[2:5]) % 3 == 0 and int(e[3:6]) % 5 == 0 and int(e[4:7]) % 7 == 0 and int(e[5:8]) % 11 == 0 and int(e[6:9]) % 13 == 0 and int(e[7:10]) % 17 == 0
+    return sum(int("".join(e)) for e in itertools.permutations(map(str, range(0, 10))) if ok("".join(e)))
+
+
+if __name__ == '__main__':
+    print(solve())
