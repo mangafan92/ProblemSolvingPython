0e36ac33ae988eb0e8cfb36245526bf4656b2d28
mangafan92 <themangafan92@gmail.com>
2016-09-04 10:50:16 +0200

    Ajout crible érathostène

---
+++ modules/eratosthenesSieve.py
@@ -0,0 +1,8 @@
+def primes(limit: int):
+    l = [True for k in range(limit)]
+
+    for i in range(2, limit):
+        if l[i] == True:
+            for j in range(2*i, limit, i):
+                l[j] = False
+            yield i
