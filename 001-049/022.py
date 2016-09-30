414956f7614e702dab9bfc0ab5eda697af36a9c1
mangafan92 <themangafan92@gmail.com>
2016-09-30 22:29:13 +0200

    022 réécrit

--- 001-049/022.py
+++ 001-049/022.py
@@ -1,21 +1,24 @@
-def scoreMot(mot):
-    lettres = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    score = 0    
-    for k in range(0, len(mot)):
-        score += lettres.index(mot[k])+1
-    return score
-    
-with open("./data/022_names.txt") as fichier:
-    fichier = fichier.read()
-    fichier = fichier.split(",")
-    for k in range(0, len(fichier)):
-        fichier[k] = fichier[k][1:len(fichier[k])-1]
-        
-    fichier.sort()
-    
-    sommeScores = 0
-    
-    for k in range(0, len(fichier)):       
-        sommeScores += (k+1)*scoreMot(fichier[k])
-    
-    print("La sommes des scores vaut", sommeScores, ".")
+import re
+
+
+def wordScore(word: str) -> int:
+    letters = list([chr(l) for l in range(ord("A"), ord("A") + 26)])
+    letterScore = lambda letter: letters.index(letter.upper()) + 1
+    return sum(map(letterScore, word))
+
+
+def contentToNames(content: str) -> list:
+    name = re.compile(r'"([A-Za-z]+)"')
+    return name.findall(content)
+
+
+def solve() -> int:
+    with open("./data/022_names.txt") as file:
+        content = file.read()
+
+    names = list(sorted(contentToNames(content)))
+    return sum((k + 1) * wordScore(name) for k, name in enumerate(names))
+
+
+if __name__ == '__main__':
+    print(solve())
