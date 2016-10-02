c91bd36b9057091af449d4b1573fdf351577e876
mangafan92 <themangafan92@gmail.com>
2016-10-02 22:09:54 +0200

    File reformated

--- 001-049/007.py
+++ 001-049/007.py
@@ -1,12 +1,14 @@
 import modules.eratosthenesSieve
 
+
 def solveProblem(number=10001):
     limit = 2
     while True:
         try:
-            return list(modules.eratosthenesSieve.primes(limit))[number-1]
+            return list(modules.eratosthenesSieve.primes(limit))[number - 1]
         except:
             limit *= 2
+
 
 if __name__ == '__main__':
     print(solveProblem())
